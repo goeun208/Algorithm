3d2fc07f8c83ff54c6944f59eab4b70325d50993
goeun208 <80875784+goeun208@users.noreply.github.com>
2023-06-07 12:20:22 +0900

    Update 문자열 다루기 기본.js

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -2,9 +2,8 @@ function solution(s) {
      if(s.length != 4 && s.length != 6) 
         return false;
     
-    if(s.includes('e')) {
+    if(s.includes('e'))
         return false;
-    }
     
     var answer = isNaN(s) ? false : true;
     return answer;
