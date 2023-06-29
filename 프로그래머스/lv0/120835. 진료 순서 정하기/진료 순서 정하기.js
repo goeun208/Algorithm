29c1166025906c979e688507eac2d33b6331fc75
goeun208 <80875784+goeun208@users.noreply.github.com>
2023-06-29 14:09:44 +0900

    Update 진료 순서 정하기.js

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,4 @@
 function solution(emergency) {
-    let sorted = emergency.slice().sort((a,b)=>b-a); // 내차순 정렬
+    let sorted = emergency.slice().sort((a,b)=>b-a); // 내림차순 정렬
     return emergency.map(v=>sorted.indexOf(v)+1);
 }
