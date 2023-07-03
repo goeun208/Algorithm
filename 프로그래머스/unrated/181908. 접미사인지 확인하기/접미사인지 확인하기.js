27011648ce4a608ea53454a9479b20e7fe9ffb5e
goeun208 <80875784+goeun208@users.noreply.github.com>
2023-07-03 10:40:30 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,7 @@
+function solution(my_string, is_suffix) {
+    var answer = 0;
+    for(let i = 0; i < my_string.length; i++) {
+        if(my_string.slice(i) === is_suffix) return 1;
+    }
+    return 0;
+}
