f2d8365baa0f9d856960cd9bbddfe1ed2be37eaa
goeun208 <80875784+goeun208@users.noreply.github.com>
2023-07-03 09:48:08 +0900

    [unrated] Title: 수 조작하기 1, Time: 5.72 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,12 @@
+function solution(n, control) {
+    let str = control.split('');
+    for(let i of str) {
+        switch(i){
+            case 'w': n = n + 1; break;
+            case 's': n = n - 1; break;
+            case 'd': n = n + 10; break;
+            case 'a': n = n - 10; break;
+        }
+    }
+    return n;
+}
