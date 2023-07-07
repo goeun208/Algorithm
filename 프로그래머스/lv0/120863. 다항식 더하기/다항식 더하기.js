6df6f7f6ae5dbd33a95ce12d1b76621784a22e37
goeun208 <80875784+goeun208@users.noreply.github.com>
2023-07-07 12:04:04 +0900

    [level 0] Title: 다항식 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -4,14 +4,12 @@ function solution(polynomial) {
     let str_count= 0; // 일차항 계수
     
     let poly_str= polynomial.split(' ');
-    for(let str of poly_str) { // poly_str
+    for(let str of poly_str) { // poly_str의 원소를 하나씩 검사
         if(str === "+") continue;
-        
-        if(str.includes("x")) //일차항
+        else if(str.includes("x")) //일차항
         {
-            console.log('x포함?', str)
             let s = str.slice(0, str.length - 1)
-                s > 1 ? str_count += Number(s) : str_count++;
+            s > 1 ? str_count += Number(s) : str_count++;
         }
         else { // 상수
             if(!isNaN(str)) const_count += Number(str);
@@ -20,20 +18,17 @@ function solution(polynomial) {
 
     if(str_count > 0) // 일차항이 존재함
     {
-        if(str_count === 1 && const_count <= 0) return "x"
-        
-        if(const_count > 0){
-            // 상수항이 존재하는가
-            if(str_count === 1) {
-                return "x" + " + " + const_count;
-            } else
-                return str_count + "x" + " + " + const_count;
+        if(str_count === 1 && const_count <= 0) return "x";
+        if(const_count < 1) return str_count + "x";
+
+        if(str_count === 1) {
+            return "x" + " + " + const_count;
         } else
-                return str_count + "x";
+            return str_count + "x" + " + " + const_count;             
     }
     else if(const_count > 0) { // 상수항만 존재함
-            return const_count + '';
+        return const_count + '';
     }
     else
-            return "0";
+        return "0";
 }
